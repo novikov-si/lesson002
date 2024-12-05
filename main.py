7ced7c9444ecbc4af0c6a7ee5cb36ed04b970deb
novikov-si <sergnovikov@gmail.com>
2024-12-06 02:13:29 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# 1st program
+print(9**0.5*5) # 15.0
+# 2nd program
+print(9.99>9.98 and 1000 != 1000.1) #True
+# 3rd program
+print(2*2+2) # 6
+print(2*(2+2)) # 8
+print(2*2+2 == 2*(2+2)) # False
+# 4th program
+print(int(float('123.456')*10)%10) # 4
